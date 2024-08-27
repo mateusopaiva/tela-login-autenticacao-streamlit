7610622197c9502e0f9d75207c4d09d5ed677f4f
Mateus Paiva <mateusopaiva@gmail.com>
2024-08-27 15:59:39 -0300

    Conexao e Interacao com PostgreSQL

--- dependencies.py
+++ dependencies.py
@@ -2,3 +2,78 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 from contextlib import contextmanager
+
+load_dotenv()
+
+DATABASE = os.getenv("DATABASE")
+HOST = os.getenv("HOST")
+USERSERVER = os.getenv("USERSERVER")
+PASSWORD = os.getenv("PASSWORD")
+PORT = os.getenv("PORT")
+
+@contextmanager
+def instance_cursor():
+  connection = psycopg2.connect(database=DATABASE, host=HOST, user=USERSERVER, password=PASSWORD, port=PORT)
+  cursor = connection.cursor()
+  try:
+    yield cursor
+  finally:
+    if (connection):
+      cursor.close()
+      connection.close()
+      print("Conexão com PostgreSQL fechada")
+  
+def consulta(user):
+  with instance_cursor() as cursor:
+    query = '''
+      SELECT nome, usuario, senha
+      FROM REGISTROS
+      WHERE usuario = %s
+    '''
+    cursor.execute(query, (user, ))
+    request = cursor.fetchall()
+    return request
+  
+def consulta_geral():
+  with instance_cursor() as cursor:
+    query = '''
+      SELECT * 
+      FROM REGISTROS
+    '''
+    cursor.execute(query, )
+    request = cursor.fetchall()
+    return request
+
+def add_registro(nome, user, senha):
+  connection = psycopg2.connect(database=DATABASE, host=HOST, user=USERSERVER, password=PASSWORD, port=PORT)
+  cursor = connection.cursor()
+
+  query = f'''
+    INSERT INTO REGISTROS VALUES
+    {nome, user, senha}
+  '''
+  cursor.execute(query)
+  connection.commit()
+  if connection:
+    cursor.close()
+    connection.close()
+    print("Conexão com PostgreSQL fechada")
+
+def cria_tabela():
+  connection = psycopg2.connect(database=DATABASE, host=HOST, user=USERSERVER, password=PASSWORD, port=PORT)
+  cursor = connection.cursor()
+
+  query = '''
+    CREATE TABLE REGISTROS (
+      nome varchar (255),
+      usuario varchar (255),
+      senha varchar (255)
+    )
+  '''
+  cursor.execute(query)
+  connection.commit()
+  print("Tabela criada")
+  if connection:
+    cursor.close()
+    connection.close()
+    print("Conexão com PostgreSQL fechada")
--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import streamlit as st
 import streamlit_authenticator as stauth
-
-COOKIE_EXPIRY_DAYS = 30
+from dependencies import consulta, consulta_geral, add_registro, cria_tabela
 
 def main():
+  try:
+    consulta_geral()
+  except:
+    cria_tabela()
+
+  db_query = consulta_geral()
+
+  registros = {'usernames': {}}
+  for data in db_query:
+    registros['usernames'][data[1]] = {'name': data[0], 'password': data[2]}
+
+  COOKIE_EXPIRY_DAYS = 30
   authenticator = stauth.Authenticate(
-    {'usernames': {'test': {'name': 'testando', 'password': 'blabla'}}},
+      registros,
      'random_cookie_name',
      'random_signature_key',
-
      COOKIE_EXPIRY_DAYS,
   )
 
@@ -17,17 +27,19 @@ def main():
   
   if st.session_state['clicou_registrar'] == False:
     login_form(authenticator=authenticator)
+  else:
+    usuario_form()
 
 def login_form(authenticator):
   name, authentication_status, username = authenticator.login('Login')
   if authentication_status:
-    authenticator.logout('Logout', main)
+    authenticator.logout('Logout', 'main')
     st.title('Área do Dashboard')
     st.write(f'*{name} está logado!')
   elif authentication_status == False:
-    st.error('Usúario/Senha incorretos.')
+    st.error('Usúario ou senha incorretos.')
   elif authentication_status == None:
-    st.error('Por favor informe um usúario e senha')
+    st.warning('Insira um nome de usuário e uma senha')
     clicou_em_registrar = st.button("Registrar")
     if clicou_em_registrar:
       st.session_state['clicou_registrar'] = True
@@ -37,10 +49,10 @@ def confirm_message():
   hashed_password = stauth.Hasher([st.session_state.password]).generate()
   if st.session_state.password != st.session_state.confirm_password:
     st.warning('Senhas não conferem')
-  elif 'consulta_nome()':
+  elif consulta(st.session_state.user):
     st.warning('Nome de usúario já existe.')
   else:
-    'add_registro()'
+    add_registro(st.session_state.nome, st.session_state.user, hashed_password[0])
     st.success('Registro efetuado!')
 
 def usuario_form():
@@ -48,12 +60,12 @@ def usuario_form():
     nome = st.text_input("Nome", key="nome")
     username = st.text_input("Usúario", key="user")
     password = st.text_input("Senha", key="password", type="password")
-    confirm_password = st.text_input("Confirme a senha", key="confirm_password", type="confirm_password")
+    confirm_password = st.text_input("Confirme a senha", key="confirm_password", type="password")
     submit = st.form_submit_button("Salvar", on_click=confirm_message)
-    clicou_em_fazer_login = st.button("Fazer Login")
-    if clicou_em_fazer_login:
-      st.session_state['clicou_registrar'] = False
-      st.rerun()
+  clicou_em_fazer_login = st.button("Fazer Login")
+  if clicou_em_fazer_login:
+    st.session_state['clicou_registrar'] = False
+    st.rerun()
 
 if __name__ == '__main__':
   main()
